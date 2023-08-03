13f1ae67d86f22c00a71601a1e67de06d4c81050
Roxana <roxana.gherman@fidasolutions.com>
2023-08-03 18:20:46 +0300

    add TeamsTable component

---
+++ src/teams/TeamsTable.tsx
@@ -0,0 +1,56 @@
+export function TeamsTable(props) {
+  // console.warn("TeamsTable", props);
+
+  return (
+    <form id="teamsForm" action="" method="get" className={props.loading ? "loading-mask" : ""}>
+      <table id="teamsTable">
+        <colgroup>
+          <col className="select-all-column" />
+          <col style={{ width: "20%" }} />
+          <col style={{ width: "40%" }} />
+          <col style={{ width: "25%" }} />
+          <col style={{ width: "15%" }} />
+          <col className="table-actions" />
+        </colgroup>
+        <thead>
+          <tr>
+            <th>
+              <input type="checkbox" name="selectAll" id="selectAll" />
+            </th>
+            <th>Promotion</th>
+            <th>Members</th>
+            <th>Project Name</th>
+            <th>Project URL</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+        <tfoot>
+          <tr>
+            <td></td>
+            <td>
+              <input type="text" name="promotion" placeholder="Enter promotion" required />
+            </td>
+            <td>
+              <input type="text" name="members" placeholder="Enter members" required />
+            </td>
+            <td>
+              <input type="text" name="name" placeholder="Enter name" required />
+            </td>
+            <td>
+              <input type="text" name="url" placeholder="Enter url" required />
+            </td>
+            <td>
+              <button type="submit" className="action-btn" title="Add">
+                ➕
+              </button>
+              <button type="reset" className="action-btn" title="Reset">
+                ✖
+              </button>
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+    </form>
+  );
+}
